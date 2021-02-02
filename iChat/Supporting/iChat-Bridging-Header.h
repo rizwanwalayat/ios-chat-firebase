1b6bc089e31dce2ae2e10e1203b1351976138e04
Rizwan Walayat <rizwan.wallayat@gmail.com>
2021-02-02 19:58:56 +0500

    Initial Setup completed

---
+++ iChat/Supporting/iChat-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  iChat-BridgingHeader.h
+//  iChat
+//
+//  Created by ps on 02/02/2021.
+//
+
+#ifndef iChat_BridgingHeader_h
+#define iChat_BridgingHeader_h
+#import "SWRevealViewController.h"
+
+#endif /* iChat_BridgingHeader_h */
