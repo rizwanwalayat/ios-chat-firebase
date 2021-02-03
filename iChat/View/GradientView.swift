b5c535c5bc0dd186077b8acb7ca7e93acb390237
Rizwan Walayat <rizwan.wallayat@gmail.com>
2021-02-03 15:34:06 +0500

    Styling

---
+++ iChat/View/GradientView.swift
@@ -0,0 +1,32 @@
+//
+//  GradientView.swift
+//  iChat
+//
+//  Created by ps on 03/02/2021.
+//
+
+import UIKit
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
