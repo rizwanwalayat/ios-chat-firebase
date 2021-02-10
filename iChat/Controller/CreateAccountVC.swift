bed52521dcd7c8e8dc4e2eb55ca688016145e708
Rizwan Walayat <rizwan.wallayat@gmail.com>
2021-02-10 13:55:22 +0500

    API Request setup for Register User

--- iChat/Controller/CreateAccountVC.swift
+++ iChat/Controller/CreateAccountVC.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
 
     }
     
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else {
+            return
+        }
+        guard let pass = passwordTxt.text, passwordTxt.text != "" else {
+            return
+        }
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("Registered User!")
+            }
+        }
+    }
     
 
 }
---
+++ iChat/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  iChat
+//
+//  Created by ps on 10/02/2021.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupRadiusView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupRadiusView()
+    }
+    
+    func setupRadiusView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+   
+}
